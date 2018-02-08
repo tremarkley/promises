9d6297d135a6c0b8b268bf8758608677e9d6d299
tremarkley <tremarkley@gmail.com>
2018-02-08 15:58:22 -0800

    Complete promisifcation.js with all tests passing

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -9,21 +9,23 @@ var crypto = require('crypto');
 var Promise = require('bluebird');
 
 // (1) Asyncronous HTTP request
-var getGitHubProfile = function(user, callback) {
-  var options = {
-    url: 'https://api.github.com/users/' + user,
-    headers: { 'User-Agent': 'request' },
-    json: true  // will JSON.parse(body) for us
-  };
+var getGitHubProfileAsync = function(user, callback) {
+  return new Promise((resolve, reject) => {
+    var options = {
+      url: 'https://api.github.com/users/' + user,
+      headers: { 'User-Agent': 'request' },
+      json: true  //will JSON.parse(body) for us
+    };
 
-  request.get(options, function(err, res, body) {
-    if (err) {
-      callback(err, null);
-    } else if (body.message) {
-      callback(new Error('Failed to get GitHub profile: ' + body.message), null);
-    } else {
-      callback(null, body);
-    }
+    request.get(options, function(err, res, body) {
+      if (err) {
+        reject(err);
+      } else if (body.message) {
+        reject(new Error('Failed to get GitHub profile: ' + body.message));
+      } else {
+        resolve(body);
+      }
+    });
   });
 };
 
@@ -31,10 +33,12 @@ var getGitHubProfileAsync; // TODO
 
 
 // (2) Asyncronous token generation
-var generateRandomToken = function(callback) {
-  crypto.randomBytes(20, function(err, buffer) {
-    if (err) { return callback(err, null); }
-    callback(null, buffer.toString('hex'));
+var generateRandomTokenAsync = function(callback) {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(20, function(err, buffer) {
+      if (err) { reject(err); }
+      resolve(buffer.toString('hex'));
+    });
   });
 };
 
@@ -42,18 +46,20 @@ var generateRandomTokenAsync; // TODO
 
 
 // (3) Asyncronous file manipulation
-var readFileAndMakeItFunny = function(filePath, callback) {
-  fs.readFile(filePath, 'utf8', function(err, file) {
-    if (err) { return callback(err); }
-   
-    var funnyFile = file.split('\n')
-      .map(function(line) {
-        return line + ' lol';
-      })
-      .join('\n');
-
-    callback(funnyFile);
+var readFileAndMakeItFunnyAsync = function(filePath, callback) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', function(err, file) {
+      if (err) { reject(err); } else {
+        var funnyFile = file.split('\n')
+          .map(function(line) {
+            return line + ' lol';
+          })
+          .join('\n');
+        resolve(funnyFile);
+      }
+    });
   });
+  
 };
 
 var readFileAndMakeItFunnyAsync; // TODO
