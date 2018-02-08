90b7d290a34ccd5cd4ce6d4e10df2faaaffd3303
tremarkley <tremarkley@gmail.com>
2018-02-08 15:39:00 -0800

    Implement getStatusCodeAsync

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,7 +10,6 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
   return new Promise((resolve, reject) => {
     var rs = fs.createReadStream(filePath, {encoding: 'utf8'});
     var line = '';
@@ -36,7 +35,15 @@ var pluckFirstLineFromFileAsync = function(filePath) {
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {
+      if (response !== undefined) {
+        resolve(response.statusCode);
+      } else {
+        reject(err);
+      }
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
